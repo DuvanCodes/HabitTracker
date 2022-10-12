8e37c82a77360b301f6472b1dcc2d32b8d942547
Blaucast <duvancc2901@gmail.com>
2022-10-12 15:52:45 -0500

    Add design UI - Part I

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import '../components/components_title.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  // data structire for todays list
+  List todaysHabitList = [
+    ["Morning Run", false],
+    ["Read Book", false],
+    ["Code App", false],
+    ["Learn Flutter", false],
+  ];
+
+  // checkbox was tapped
+  void checkBoxTapped(bool? value, int index) {
+    setState(() {
+      todaysHabitList[index][1] = value;
+    });
+  }
+
+  // create a new habit
+  void createNewHabit() {
+    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: ListView.builder(
+        itemCount: todaysHabitList.length,
+        itemBuilder: (context, index) {
+          return HabitTitle(
+            habitName:  todaysHabitList[index][0],
+            habitCompleted: todaysHabitList[index][1],
+            onChanged: (value) => checkBoxTapped(value, index),
+          );
+        },
+      ),
+    );
+  }
+}
